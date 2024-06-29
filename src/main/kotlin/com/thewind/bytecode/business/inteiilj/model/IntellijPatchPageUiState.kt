74103e823dceeeebc7446513575866fedeaffaea
bestyize <3011429848@qq.com>
2024-06-29 15:18:31 +0800

    auto replace file

--- src/main/kotlin/com/thewind/bytecode/business/inteiilj/model/IntellijPatchPageUiState.kt
+++ src/main/kotlin/com/thewind/bytecode/business/inteiilj/model/IntellijPatchPageUiState.kt
@@ -7,7 +7,7 @@ data class IntellijPatchPageUiState(
     val installPath: String = "",
     val notices: List<String> = listOf("输入安装地址"),
     val pathHint:String = """
-        点击此处输入安装地址，例如\n" + "/home/read/.local/share/JetBrains/Toolbox/apps/intellij-idea-ultimate/
+        点击此处输入安装地址，例如: /home/read/.local/share/JetBrains/Toolbox/apps/intellij-idea-ultimate/
     """.trimIndent(),
     val isPatching: Boolean = false
 )
